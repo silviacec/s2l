f2f3f262394075596d3f4d043ef42f7abb97b1f0
Cek <silviacecuttimsa@gmail.com>
2020-08-21 11:46:40 +0200

    add loop for three projects in Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,23 +2,26 @@
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <div><Steps /></div>
-    <Pitch />
-    <!-- <Project :entreprise="entreprise"/> -->
-
+    <!-- <Pitch /> -->
+    <div class="projets" v-for="entreprise in entreprises" :key="entreprise.id">
+      <div v-if="entreprise.firstPage" >
+        <Project :entreprise="entreprise"></Project>
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Pitch from "@/components/Pitch.vue";
-// import Project from "@/components/Project.vue";
+// import Pitch from "@/components/Pitch.vue";
+import Project from "@/components/Project.vue";
 import Steps from "@/components/Steps.vue";
 import {entreprises} from "../data.js";
 
 export default {
   name: "Home",
   components: {
-    Pitch, Steps
+    Steps, Project
   },
 
   data() {
@@ -27,10 +30,10 @@ export default {
     };
   },
 
-  methods: {
-    voirProjet(identifiant) {
-      this.$router.push({ path: `/SingleProject/${identifiant}` });
-    }
-  },
+  // methods: {
+  //   voirProjet(identifiant) {
+  //     this.$router.push({ path: `/SingleProject/${identifiant}` });
+  //   }
+  // },
 };
 </script>
