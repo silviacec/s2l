0a5fc0e682b39819aea4f59e8be0031f2f05d8cc
Cek <silviacecuttimsa@gmail.com>
2020-08-24 15:26:19 +0200

    fix vue version

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
-    <!-- <div><Steps /></div> -->
-    <!-- <Pitch /> -->
+    <Steps />
+    <Pitch />
     <div class="projets" v-for="entreprise in entreprises" :key="entreprise.id">
       <!-- <div v-if="entreprise.firstPage" > -->
         <Project :entreprise="entreprise"></Project>
@@ -14,16 +14,16 @@
 
 <script>
 // @ is an alias to /src
-// import Pitch from "@/components/Pitch.vue";
+import Pitch from "@/components/Pitch.vue";
 import Project from "@/components/Project.vue";
-// import Steps from "@/components/Steps.vue";
+import Steps from "@/components/Steps.vue";
 import {entreprises} from "../data.js";
 // import Rounds from "@/components/Rounds.vue";
 
 export default {
   name: "Home",
   components: {
-    Project
+    Project, Pitch, Steps
   },
 
   data() {
